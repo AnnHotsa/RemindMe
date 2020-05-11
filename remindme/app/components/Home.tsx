6ed8498a1a6e74d7662a6e0f593b9a9628c5867d
Anna Hotsa <annahotsa@gmail.com>
2020-05-12 00:52:15 +0300

    set up project architecture

---
+++ remindme/app/components/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import routes from '../constants/routes.json';
+import styles from './Home.css';
+
+export default function Home() {
+  return (
+    <div className={styles.container} data-tid="container">
+      <h2>RemindMe</h2>
+      <Link to={routes.COUNTER}>to Counter</Link>
+    </div>
+  );
+}
